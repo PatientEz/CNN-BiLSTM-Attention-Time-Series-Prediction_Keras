4ef335a3fac365bd0dd9b5e18a4c164965f10b5f
HoloLensCnDev <hololenschina@microsoft.com>
2020-03-18 13:19:31 +0800

    first commit

---
+++ Main.py
@@ -0,0 +1,132 @@
+from keras.layers import Input, Dense, LSTM, merge ,Conv1D,Dropout,Bidirectional,Multiply
+from keras.models import Model
+
+
+from attention_utils import get_activations
+from keras.layers import merge
+from keras.layers.core import *
+from keras.layers.recurrent import LSTM
+from keras.models import *
+
+import  pandas as pd
+import  numpy as np
+
+
+
+
+
+SINGLE_ATTENTION_VECTOR = False
+def attention_3d_block(inputs):
+    # inputs.shape = (batch_size, time_steps, input_dim)
+    input_dim = int(inputs.shape[2])
+    a = inputs
+    #a = Permute((2, 1))(inputs)
+    #a = Reshape((input_dim, TIME_STEPS))(a) # this line is not useful. It's just to know which dimension is what.
+    a = Dense(input_dim, activation='softmax')(a)
+    if SINGLE_ATTENTION_VECTOR:
+        a = Lambda(lambda x: K.mean(x, axis=1), name='dim_reduction')(a)
+        a = RepeatVector(input_dim)(a)
+    a_probs = Permute((1, 2), name='attention_vec')(a)
+
+    output_attention_mul = merge([inputs, a_probs], name='attention_mul', mode='mul')
+    return output_attention_mul
+
+def create_dataset(dataset, look_back):
+    '''
+    对数据进行处理
+    '''
+    dataX, dataY = [], []
+    for i in range(len(dataset)-look_back-1):
+        a = dataset[i:(i+look_back),:]
+        dataX.append(a)
+        dataY.append(dataset[i + look_back,:])
+    TrainX = np.array(dataX)
+    Train_Y = np.array(dataY)
+
+    return TrainX, Train_Y
+
+#多维归一化  返回数据和最大最小值
+def NormalizeMult(data):
+    #normalize 用于反归一化
+    data = np.array(data)
+    normalize = np.arange(2*data.shape[1],dtype='float64')
+
+    normalize = normalize.reshape(data.shape[1],2)
+    print(normalize.shape)
+    for i in range(0,data.shape[1]):
+        #第i列
+        list = data[:,i]
+        listlow,listhigh =  np.percentile(list, [0, 100])
+        # print(i)
+        normalize[i,0] = listlow
+        normalize[i,1] = listhigh
+        delta = listhigh - listlow
+        if delta != 0:
+            #第j行
+            for j in range(0,data.shape[0]):
+                data[j,i]  =  (data[j,i] - listlow)/delta
+    #np.save("./normalize.npy",normalize)
+    return  data,normalize
+
+#多维反归一化
+def FNormalizeMult(data,normalize):
+    data = np.array(data)
+    for i in  range(0,data.shape[1]):
+        listlow =  normalize[i,0]
+        listhigh = normalize[i,1]
+        delta = listhigh - listlow
+        if delta != 0:
+            #第j行
+            for j in range(0,data.shape[0]):
+                data[j,i]  =  data[j,i]*delta + listlow
+
+    return data
+
+
+def attention_model():
+    inputs = Input(shape=(TIME_STEPS, INPUT_DIMS))
+
+    x = Conv1D(filters = 64, kernel_size = 1, activation = 'relu')(inputs)  #, padding = 'same'
+    x = Dropout(0.3)(x)
+
+    #lstm_out = Bidirectional(LSTM(lstm_units, activation='relu'), name='bilstm')(x)
+    lstm_out = Bidirectional(LSTM(lstm_units, return_sequences=True))(x)
+    attention_mul = attention_3d_block(lstm_out)
+    attention_mul = Flatten()(attention_mul)
+
+    output = Dense(1, activation='sigmoid')(attention_mul)
+    model = Model(inputs=[inputs], outputs=output)
+    return model
+
+
+
+
+#加载数据
+
+data = pd.read_csv("./pollution.csv")
+
+data = data.drop(['date','wnd_dir'], axis = 1)
+
+print(data.columns)
+print(data.shape)
+
+
+INPUT_DIMS = 7
+TIME_STEPS = 100
+lstm_units = 140
+
+#归一化
+data,normalize = NormalizeMult(data)
+pollution_data = data[:,0].reshape(len(data),1)
+
+train_X, _ = create_dataset(data,TIME_STEPS)
+_ , train_Y = create_dataset(pollution_data,TIME_STEPS)
+
+print(train_X.shape,train_Y.shape)
+
+m = attention_model()
+m.summary()
+m.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+m.fit([train_X], train_Y, epochs=1, batch_size=64, validation_split=0.1)
+#m.save("./model.h5")
+#np.save("normalize.npy",normalize)
---
+++ README.md
@@ -0,0 +1,7 @@
+TensorFlow Version:1.9.0
+
+Keras Version:2.0.2
+
+My Blog: https://blog.csdn.net/qq_35649669/article/details/104793484
+
+![layers](./Img/layers.png)
